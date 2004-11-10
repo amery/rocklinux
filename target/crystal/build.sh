38b5017f21611b2e690a4d244ab721c5bac2b2ee
Clifford Wolf <clifford@clifford.at>
2004-11-10 13:21:53 +0000

    Clifford Wolf: 	Removed doc packages from crystal 	Incrased default ISO size to 680 MB
    [2004110215050626874] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://www.rocklinux.org/svn/rock-linux/trunk@4769 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/crystal/build.sh
+++ target/crystal/build.sh
@@ -7,10 +7,18 @@ pkgloop
 
 echo_header "Finishing build."
 
+echo_status "Selecting bin packages ..."
+rm -rf build/${ROCKCFG_ID}/ROCK/pkgs_sel
+mkdir -p build/${ROCKCFG_ID}/ROCK/pkgs_sel
+ln build/${ROCKCFG_ID}/ROCK/pkgs/* build/${ROCKCFG_ID}/ROCK/pkgs_sel/
+
+# :doc packages are nice but in most cases never used
+rm -f build/${ROCKCFG_ID}/ROCK/pkgs_sel/*:doc{-*,}.gem
+
 echo_status "Creating package database ..."
 admdir="build/${ROCKCFG_ID}/var/adm"
-create_package_db $admdir build/${ROCKCFG_ID}/ROCK/pkgs \
-                  build/${ROCKCFG_ID}/ROCK/pkgs/packages.db
+create_package_db $admdir build/${ROCKCFG_ID}/ROCK/pkgs_sel \
+                  build/${ROCKCFG_ID}/ROCK/pkgs_sel/packages.db
 
 echo_status "Creating isofs.txt file .."
 cat << EOT > build/${ROCKCFG_ID}/ROCK/isofs.txt
@@ -21,7 +29,7 @@ DISK1	$admdir/descs/					${ROCKCFG_SHORTID}/info/descs/
 DISK1	$admdir/flists/					${ROCKCFG_SHORTID}/info/flists/
 DISK1	$admdir/md5sums/				${ROCKCFG_SHORTID}/info/md5sums/
 DISK1	$admdir/packages/				${ROCKCFG_SHORTID}/info/packages/
-EVERY	build/${ROCKCFG_ID}/ROCK/pkgs/packages.db	${ROCKCFG_SHORTID}/pkgs/packages.db
-SPLIT	build/${ROCKCFG_ID}/ROCK/pkgs/			${ROCKCFG_SHORTID}/pkgs/
+EVERY	build/${ROCKCFG_ID}/ROCK/pkgs_sel/packages.db	${ROCKCFG_SHORTID}/pkgs/packages.db
+SPLIT	build/${ROCKCFG_ID}/ROCK/pkgs_sel/		${ROCKCFG_SHORTID}/pkgs/
 EOT
 

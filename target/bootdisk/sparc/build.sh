41b49b87635fd7929715339b9e901fee3259965e
fake <fake@rapidnetworks.de>
2004-12-13 16:44:35 +0000

    fake: 	bootdisk/sparc: remove unused if statement
    [2004120913575832381] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://www.rocklinux.org/svn/rock-linux/trunk@5175 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/bootdisk/sparc/build.sh
+++ target/bootdisk/sparc/build.sh
@@ -1,33 +1,28 @@
 
-use_silo=1
-
 cd $disksdir
 
 echo_header "Cleaning boot directory:"
 rm -rfv boot/*-rock boot/System.map boot/kconfig* boot/initrd*img boot/*.b
 
-if [ $use_silo -eq 1 ]
-then
-	echo_header "Creating silo setup:"
-	#
-	echo_status "Extracting silo boot loader images."
-	mkdir -p boot
-	tar --use-compress-program=bzip2 \
-	    -xf $base/build/${ROCKCFG_ID}/ROCK/pkgs/silo.tar.bz2 \
-	    boot/second.b -O > boot/second.b
-	#
-	echo_status "Creating silo config file."
-	cp -v $base/target/$target/sparc/{silo.conf,boot.msg,help1.txt} \
-	  boot
-	#
-	echo_status "Moving image (initrd) to boot directory."
-	mv -v initrd.gz boot/
-	#
-	buildroot="build/${ROCKCFG_ID}"
-	datadir="build/${ROCKCFG_ID}/ROCK/bootdisk"
-	cat > ../isofs_arch.txt <<- EOT
-		BOOT	-G $buildroot/boot/isofs.b -B ...
-		DISK1	$datadir/boot/ boot/
-	EOT
-fi
+echo_header "Creating silo setup:"
+#
+echo_status "Extracting silo boot loader images."
+mkdir -p boot
+tar --use-compress-program=bzip2 \
+    -xf $base/build/${ROCKCFG_ID}/ROCK/pkgs/silo.tar.bz2 \
+    boot/second.b -O > boot/second.b
+#
+echo_status "Creating silo config file."
+cp -v $base/target/$target/sparc/{silo.conf,boot.msg,help1.txt} \
+  boot
+#
+echo_status "Moving image (initrd) to boot directory."
+mv -v initrd.gz boot/
+#
+buildroot="build/${ROCKCFG_ID}"
+datadir="build/${ROCKCFG_ID}/ROCK/bootdisk"
+cat > ../isofs_arch.txt <<- EOT
+	BOOT	-G $buildroot/boot/isofs.b -B ...
+	DISK1	$datadir/boot/ boot/
+EOT
 

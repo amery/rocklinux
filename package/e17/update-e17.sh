f5a03f725be21082a75d9cad81458a7532f696f7
Clifford Wolf <clifford@clifford.at>
2005-07-27 10:38:38 +0000

    Clifford Wolf: 	Fixed 'update-e17.sh' to use yesterdays CVS checkout 	(todays version is still changeing)
    [2005072712331915447] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://www.rocklinux.org/svn/rock-linux/trunk@6271 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/e17/update-e17.sh
+++ package/e17/update-e17.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "Updating packages in the e17 repository to version date $(date +%Y-%m-%d)"
+echo "Updating packages in the e17 repository to version date $(date --date '-1 day' '+%Y-%m-%d')"
 
 for pkg in $(cd package/e17; echo *); do
     if [ -x "package/e17/$pkg" ]; then
-	./scripts/Create-PkgUpdPatch $pkg-$(date +%Y-%m-%d) | patch -p0
+	./scripts/Create-PkgUpdPatch $pkg-$(date --date '-1 day' '+%Y-%m-%d') | patch -p0 -bz.orig
     fi
 done
 

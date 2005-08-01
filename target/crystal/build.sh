dead7656b6fcef7f27f855d2d2860f713754d87c
Clifford Wolf <clifford@clifford.at>
2005-08-01 16:07:47 +0000

    Clifford Wolf: 	Do not include linux source package on crystal ISOs 	Do not include metadata from add-on packages on crystal ISOs
    [2005080116125318840] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://www.rocklinux.org/svn/rock-linux/trunk@6292 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- target/crystal/build.sh
+++ target/crystal/build.sh
@@ -25,19 +25,37 @@ if [ "$ROCKCFG_TARGET_CRYSTAL_BUILDADDONS" = 1 ]; then
 	done
 fi
 
+# do not include the linux source package on the install media
+rm build/${ROCKCFG_ID}/ROCK/pkgs_sel/linux2?-source-*.gem
+
+echo_status "Selecting info files ..."
+rm -rf build/${ROCKCFG_ID}/ROCK/info_sel
+mkdir -p build/${ROCKCFG_ID}/ROCK/info_sel
+cp -rl build/${ROCKCFG_ID}/var/adm/`
+	`{cksums,dependencies,descs,flists,md5sums,packages} \
+	build/${ROCKCFG_ID}/ROCK/info_sel/.
+
+grep -hr '^Package Name and Version:' \
+	build/${ROCKCFG_ID}/ROCK/info_sel/packages |
+awk '{ print $5 " " $6 "-" $7; }' |
+while read p v; do
+	if [ ! -f build/${ROCKCFG_ID}/ROCK/pkgs_sel/$p-$v.gem ]; then
+		echo build/${ROCKCFG_ID}/ROCK/info_sel/*/$p
+	fi
+done | xargs -r rm
+
 echo_status "Creating package database (everything) ..."
 admdir="build/${ROCKCFG_ID}/var/adm"
 create_package_db $admdir build/${ROCKCFG_ID}/ROCK/pkgs \
                   build/${ROCKCFG_ID}/ROCK/pkgs/packages.db
 
 echo_status "Creating package database (install media) ..."
-admdir="build/${ROCKCFG_ID}/var/adm"
+admdir="build/${ROCKCFG_ID}/ROCK/info_sel"
 create_package_db $admdir build/${ROCKCFG_ID}/ROCK/pkgs_sel \
                   build/${ROCKCFG_ID}/ROCK/pkgs_sel/packages.db
 
 echo_status "Creating isofs.txt file .."
 cat << EOT > build/${ROCKCFG_ID}/ROCK/isofs.txt
-DISK1	$admdir/cache/					${ROCKCFG_SHORTID}/info/cache/
 DISK1	$admdir/cksums/					${ROCKCFG_SHORTID}/info/cksums/
 DISK1	$admdir/dependencies/				${ROCKCFG_SHORTID}/info/dependencies/
 DISK1	$admdir/descs/					${ROCKCFG_SHORTID}/info/descs/

bb51d5f27387f6b956ba3fc3cdbaa00e74aeb90b
Sebastian Knapp <rock@ccls-online.de>
2005-09-14 16:33:02 +0000

    giftnuss:   rediff of 1818184620576 with stack protector   applay-protector.sh edited because .dif filename   changes
    Index: package/base/gcc/gcc34/head-tail.patch
    ===================================================================
    
    [2005082723334526584] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://www.rocklinux.org/svn/rock-linux/trunk@6354 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/base/gcc/apply-protector.sh
+++ package/base/gcc/apply-protector.sh
@@ -3,7 +3,9 @@ pfile=$( echo "$3" | grep protector | tr ' ' '\t' | tr -s '\t' | cut -f2 )
 
 if [ "$pfile" ]
 then
-	tar --use-compress-program=bzip2 -xf $archdir/${pfile/.gz/.bz2}
+        tar='tar --use-compress-program=bzip2'
+        pdif=$($tar -tf $archdir/${pfile/.gz/.bz2} | grep '\.dif$')
+	$tar -xf $archdir/${pfile/.gz/.bz2}
 
 	# Patch protector.dif a bit to apply against current gcc-3
 	if test -f $1/package/base/gcc/$2/protector-hotfix.diff; then
@@ -14,7 +16,7 @@ then
 	# Set -fstack-protector as default?
 	# [ $ROCKCFG_PKG_GCC[23]_STACKPRO = 1 ] && patch -p0 < protectonly.dif
 
-	patch -p0 < protector.dif
+	patch -p0 < $pdif
 	if [ $2 != gcc34 ]; then
 		mv protector.{c,h} gcc/
 	fi

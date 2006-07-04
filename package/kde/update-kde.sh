b6c32b2b2376413edbdaefa475537c584f4c9f34
Stefan Fiedler <stefan.fiedler@students.jku.at>
2006-07-04 19:21:45 +0000

    Stefan Fiedler: 	update kde (3.5.3) 	also update arts in update-kde.sh
    [2006060815090716310] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://www.rocklinux.org/svn/rock-linux/trunk@7698 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/kde/update-kde.sh
+++ package/kde/update-kde.sh
@@ -6,9 +6,14 @@ if [ ! "$1" -o ! "$2" ] ; then
 fi
 
 for x in kde* ; do
-	echo "Updateing $x ..."
+	echo "Updating $x ..."
 	sed 	-e s,$1,$2,g \
 		-e "s/\[D\] [0-9]* /\[D\] 0 /" $x/$x.desc > $x/$x.desc.new
 	mv $x/$x.desc.new $x/$x.desc
 done
 
+x="arts"
+echo "Updating $x ..."
+sed 	-e s,1.${1:2},1.${2:2},g \
+	-e "s/\[D\] [0-9]* /\[D\] 0 /" $x/$x.desc > $x/$x.desc.new
+mv $x/$x.desc.new $x/$x.desc

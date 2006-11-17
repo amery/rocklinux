236f322f4421f82fd51e88eb616b6cf6509f19f3
Stefan Fiedler <stefan.fiedler@students.jku.at>
2006-11-17 09:53:20 +0000

    Stefan Fiedler: 	update gnome2 repository (part 1); 	improve gnome-update.sh script. 	This is WORK IN PROGRESS; there might be obsolete patches and 	other things to fix.
    [2006100515143226450] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://www.rocklinux.org/svn/rock-linux/trunk@8010 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/gnome2/update-gnome.sh
+++ package/gnome2/update-gnome.sh
@@ -20,8 +20,11 @@ rev="${ver##*.}"
 baseurl="http://ftp.gnome.org/pub/GNOME"
 urlplatform="$baseurl/platform/$major/$major.$rev/sources/"
 urldesktop="$baseurl/desktop/$major/$major.$rev/sources/"
+urlbindings="$baseurl/bindings/$major/$major.$rev/sources"
+urlbindings="$urlbindings/c++/ $urlbindings/java/ $urlbindings/mono/ $urlbindings/python/"
+urladmin="$baseurl/admin/$major/$major.$rev/sources/"
 
-for url in $urlplatform $urldesktop; do
+for url in $urlplatform $urldesktop $urlbindings $urladmin; do
 	wget -q -O - $url | \
 	sed -n '/id="body"/,/\/div/{/tar.bz2/p}' | \
 	sed -r 's/^.*href="([^"]*).tar.bz2".*$/\1/' | \
@@ -39,6 +42,48 @@ for url in $urlplatform $urldesktop; do
 			fi
 		else
 			echo "$pkg is not a rock package yet"
+# 			echo "creating $pkg package"
+# 			bash misc/archive/newpackage.sh package/gnome2/$pkg $url$newver.tar.bz2
 		fi
+		if [ -e package/*/$pkg/$pkg.desc -a $dry = 0 ] ; then
+			sed -i -e "s,^\(\[D\] .*$newver.* \)http://.*,\1$url,g" package/*/$pkg/$pkg.desc
+		fi
+	done
+done
+
+grep "^\[D\] .* http://ftp.gnome.org/pub/GNOME/sources/" package/gnome2/*/*.desc | \
+while IFS=":" read pkg download; do
+ 	pkg=${pkg##*/} ; pkg=${pkg%.desc}
+
+	read dtag cksum file durl < <(echo $download)
+	durl="${durl%/}" ; durl="${durl%/*}"
+
+	wget -q -O - $durl | \
+	sed -n -e 's,.*href="\([0-9.].*\)".*,\1,gp' | \
+	tail -n1 | \
+	while read newmajor ; do
+		url="$durl/$newmajor"
+
+		wget -q -O - $url | \
+		sed -n -e"s,.*\(LATEST-IS-[^<]*\).*,\1,gp" | \
+		while read newver; do
+			pkgver="${newver#LATEST-IS-}"
+
+			if [ -d package/*/$pkg ]; then
+				if [ $dry = 1 ] ; then
+					echo $pkg-$pkgver
+				else
+					./scripts/Create-PkgUpdPatch $pkg-$pkgver | \
+					patch -p0
+				fi
+			else
+				echo "$pkg is not a rock package yet"
+# 				echo "creating $pkg package"
+# 				bash misc/archive/newpackage.sh package/gnome2/$pkg $url$newver.tar.bz2
+			fi
+			if [ -e package/*/$pkg/$pkg.desc -a $dry = 0 ] ; then
+				sed -i -e "s,^\(\[D\] .*$pkgver.* \)http://.*,\1$url,g" package/*/$pkg/$pkg.desc
+			fi
+	 	done
 	done
 done

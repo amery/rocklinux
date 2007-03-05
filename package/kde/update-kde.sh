4ecdc0ed490f7e1f8212f5f204e30e26ca40186d
Stefan Fiedler <stefan.fiedler@students.jku.at>
2007-03-05 00:32:21 +0000

    Stefan Fiedler: 	update kde (3.5.5 -> 3.5.6) 	improve update-kde.sh a bit 	UNTESTED
    [2007020309525414264] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://www.rocklinux.org/svn/rock-linux/trunk@8233 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/kde/update-kde.sh
+++ package/kde/update-kde.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+(
+oldver="${1//./\.}"
+newver="${2//./\.}"
+
+cd package/kde
+
 if [ ! "$1" -o ! "$2" ] ; then
 	echo "You must specify old and new version ..."
 	exit -1
@@ -8,7 +14,7 @@ fi
 for x in kde* arts ; do
 	[ -f $x/$x.desc ] || continue
 	echo "Updating $x ..."
-	sed 	-e s,$1,$2,g \
+	sed 	-e "s,$oldver,$newver,g" \
 		-e "s/\[D\] [0-9]* /\[D\] 0 /" $x/$x.desc > $x/$x.desc.new
 	mv $x/$x.desc.new $x/$x.desc
 done
@@ -17,6 +23,14 @@ done
 # the arts.desc is changed above and below.
 x="arts"
 echo "Updating $x ..."
-sed 	-e s,1.${1:2},1.${2:2},g \
+sed 	-e "s,1\.${oldver:3},1\.${newver:3},g" \
 	-e "s/\[D\] [0-9]* /\[D\] 0 /" $x/$x.desc > $x/$x.desc.new
 mv $x/$x.desc.new $x/$x.desc
+
+# Likewise for kdevelop, but the version number is 3.3.x.
+x="kdevelop"
+echo "Updating $x ..."
+sed 	-e "s,3\.3\.${oldver:6},3\.3\.${newver:6},g" \
+	-e "s/\[D\] [0-9]* /\[D\] 0 /" $x/$x.desc > $x/$x.desc.new
+mv $x/$x.desc.new $x/$x.desc
+)

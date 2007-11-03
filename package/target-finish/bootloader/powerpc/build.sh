b33b9fe2cd76b2dad69d0c86dd0b60ffe44c31f8
Stefan Fiedler <stefan.fiedler@students.jku.at>
2007-11-03 09:25:58 +0000

    Stefan Fiedler: 	yaboot: update and fix for cross-builds 	target-finish/bootloader: fix for powerpc 	This makes powerpc isos bootable again
    [2007091909324430312] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://www.rocklinux.org/svn/rock-linux/trunk@8786 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

--- package/target-finish/bootloader/powerpc/build.sh
+++ package/target-finish/bootloader/powerpc/build.sh
@@ -3,8 +3,10 @@ use_yaboot=1
 
 cd $disksdir
 
-echo "Creating cleaning boot directory:"
-rm -rfv boot/*-rock boot/System.map boot/kconfig*
+# yaboot doesn't seem to like the symlink.
+echo "Renaming kernel image:"
+rm -vf boot/vmlinux
+mv -vf boot/vmlinux_* boot/vmlinux
 
 if [ $use_yaboot -eq 1 ]
 then
@@ -12,11 +14,9 @@ then
 	#
 	echo "Extracting yaboot boot loader images."
 	mkdir -p boot etc
-	tar -O $taropt $base/build/${ROCKCFG_ID}/ROCK/pkgs/yaboot.tar.bz2 \
-	    usr/lib/yaboot/yaboot > boot/yaboot
-	tar -O $taropt $base/build/${ROCKCFG_ID}/ROCK/pkgs/yaboot.tar.bz2 \
-            usr/lib/yaboot/yaboot.rs6k > boot/yaboot.rs6k
-	cp boot/yaboot.rs6k install.bin
+	cp -v $root/usr/lib/yaboot/yaboot boot/
+	cp -v $root/usr/lib/yaboot/yaboot.rs6k boot/
+	cp -v boot/yaboot.rs6k install.bin
 	#
 	echo "Creating yaboot config files."
 	cp -v $confdir/powerpc/{boot.msg,ofboot.b} \
@@ -30,9 +30,6 @@ then
 		cat $confdir/powerpc/yaboot.conf
 	) > boot/yaboot.conf
 	#
-	echo "Moving image (initrd) to boot directory."
-	mv -v initrd.gz boot/
-	#
 	echo "Copy more config files."
 	cp -v $confdir/powerpc/mapping .
 	#
@@ -41,7 +38,6 @@ then
 		BOOT	-hfs -part -map $datadir/mapping -hfs-volid "ROCK_Linux_CD"
 		BOOTx	-hfs-bless boot -sysid PPC -l -L -r -T -chrp-boot
 		BOOTx   --prep-boot install.bin
-		DISK1	$datadir/boot/ boot/
 		DISK1	$datadir/etc/ etc/
 		DISK1	$datadir/install.bin install.bin
 	EOT

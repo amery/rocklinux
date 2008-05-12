92719c9a38079e763a0c68c9a1b736b337158891
Stefan Fiedler <stefan.fiedler@students.jku.at>
2008-05-12 09:54:50 +0000

    Stefan Fiedler: 	add 2nd-stage-livecd and initramfs target-finish packages
    [2007112923010260950] (https://www.rocklinux.net/submaster)
    
    
    
    git-svn-id: http://www.rocklinux.org/svn/rock-linux/trunk@8979 c5f82cb5-29bc-0310-9cd0-bff59a50e3bc

---
+++ package/target-finish/initramfs/files/initramfs-livecd/linuxrc.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+echo "$0: $*"
+
+PATH="/bin:/usr/bin:/sbin:/usr/sbin"
+export PATH
+
+for x in /init.d/*
+do
+	. $x
+done
+while : ; do
+	echo "going real..."
+	if [ -x /real-root/sbin/init ] ; then
+		#exec /sbin/run_init /real-root /sbin/init
+		
+# 		mount --move  /dev /real-root/dev
+# 		mount --move  /proc /real-root/proc
+# 		mount --move /sys /real-root/sys
+
+		mkdir -p /real-root/branch/irfs
+		mount --bind / /real-root/branch/irfs
+
+		cd /real-root
+		sed -e "s,/*real-root/*,/,g" /etc/mtab > etc/mtab
+		exec chroot . sh -c "exec /sbin/init" <dev/console >dev/console 2>&1
+	elif [ -x /sbin/init ] ; then
+		exec /sbin/init <dev/console >dev/console 2>&1
+	fi
+	echo "no /real-root/sbin/init found. spawning /bin/bash..."
+	/bin/bash
+done
